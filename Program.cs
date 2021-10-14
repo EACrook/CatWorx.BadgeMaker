0f86d416ed168b83c5dc5c63ab48e3ea176af7b3
Emily Crook <Emily.A.Crook96@gmail.com>
2021-10-14 13:41:42 -0500

    created an employee class and organized code into separate methods

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CatWorx.BadgeMaker
+{
+    class Program
+    {
+        static List<string> GetEmployees() {
+            // I will return a List of strings
+            List<string> employees = new List<string>();
+            // Collect user values until the value is an empty string
+           while (true)
+            {
+                Console.WriteLine("Please enter a name: (leave empty to exit): ");
+                // Get a name from the console and assign it to a variable
+                string input = Console.ReadLine();
+                // Break if the user hits ENTER without typing a name
+                if (input == "") {
+                    break;
+                }
+                // Create a new Employee instance
+                Employee currentEmployee = new Employee();
+                employees.Add(input);
+            }
+            // This is important!!!
+            return employees;
+        }
+
+        static void PrintEmployees(List<string> employees) {
+            // Loop over the list of employees and print them in the console
+            for(int i = 0; i < employees.Count; i++) {
+                Console.WriteLine(employees[i]);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            // This will get the employees from the following method
+           List<string> employees = GetEmployees();
+           PrintEmployees(employees);
+        }
+    }
+}
